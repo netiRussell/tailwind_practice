1f6aa1730b383fb9bb8db1242a4a494807c9be72
netiRussell <forworkneti@gmail.com>
2022-10-29 13:33:55 -0700

    Mobile menu functionality

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const mobile_opener = document.getElementById("mobile_open");
+
+if (mobile_opener) {
+  const mobile_closer = document.getElementById("mobile_close");
+  const mobile_menu = document.getElementById("mobile_menu");
+
+  const mobile_functionality = function () {
+    mobile_menu.classList.toggle("flex");
+    mobile_menu.classList.toggle("hidden");
+  };
+
+  mobile_opener.addEventListener("click", mobile_functionality);
+
+  mobile_closer.addEventListener("click", mobile_functionality);
+}
